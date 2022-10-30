2450409c7bbf20bb4ba2670d179c7ed8c5c1c12b
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-10-30 09:15:16 -0400

    finished styling

---
+++ src/components/apis/YouTube.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import axios from 'axios';
+
+const KEY = 'AIzaSyDEmpKMPW39e3QO6knm0wMFAqplO6d7DnM';
+const BASE = 'https://www.googleapis.com/youtube/v3';
+
+export default axios.create({
+    baseURL : BASE,
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
+});
+
